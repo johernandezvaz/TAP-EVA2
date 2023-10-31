deee98a693de5a44baf50e2faa4c6f389bac7163
José de Jesús Hernández Vázquez <142260720+johernandezvaz@users.noreply.github.com>
2023-10-30 20:38:42 -0400

    Add files via upload

---
+++ EVA2_10_USAR_LIBRERIA/src/eva2_10_usar_libreria/EVA2_10_USAR_LIBRERIA.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_10_usar_libreria;
+
+import mis_librerias.MisLibrerias;
+/**
+ *
+ * @author Josep
+ */
+public class EVA2_10_USAR_LIBRERIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Favrotial de 5 = " + MisLibrerias.calculaFacto(5));
+    }
+    
+}
